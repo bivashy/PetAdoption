2437816c9d82e4221a1bc54b3e16bd66815de769
Bokeykhan Batyrgali <botyrbojey@gmail.com>
2025-02-21 22:23:08 +0500

    Implement barebone codebase

---
+++ src/test/java/com/bivashy/learn/pet/PetAdoptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bivashy.learn.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
